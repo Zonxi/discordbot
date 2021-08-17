e00992c1b6ab5bd018e9ceea585865901b46ff92
Zonxi <84424787+Zonxi@users.noreply.github.com>
2021-08-17 11:38:50 -0400

    Delete models directory

--- models/warndb.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose')
-
-const warndb = new mongoose.Schema({
-    guild: String,
-    user: String,
-    content: Array //lets try again
-})
-
-module.exports = mongoose.model("warndb", warndb);
